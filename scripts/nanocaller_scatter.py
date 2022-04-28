5d1c10ee1e52ce4ea4d715ed48c538778dd1fe9c
Miller <millerd15@email.chop.edu>
2022-04-28 13:37:22 -0400

    :tada: manual scattering of nanocaller

--- scripts/nanocaller_scatter.py
+++ scripts/nanocaller_scatter.py
@@ -3,6 +3,7 @@
 import argparse
 import datetime
 import os
+import pysam
 import sys
 from subprocess import PIPE, Popen
 
@@ -41,7 +42,6 @@ if __name__ == '__main__':
     region_group.add_argument("-include_bed",  "--include_bed",  help="Only call variants inside the intervals specified in the bgzipped and tabix indexed BED file. If any other flags are used to specify a region, intersect the region with intervals in the BED file, e.g. if -chom chr1 -start 10000000 -end 20000000 flags are set, call variants inside the intervals specified by the BED file that overlap with chr1:10000000-20000000. Same goes for the case when whole genome variant calling flag is set.", type=str, default=None)
     region_group.add_argument('-wgs_contigs_type','--wgs_contigs_type', \
                         help="""Options are "with_chr", "without_chr" and "all",\
-                        "with_chr" option will assume \
                         "with_chr" option will assume \
                         human genome and run NanoCaller on chr1-22, "without_chr" will \
                         run on chromosomes 1-22 if the BAM and reference genome files \
@@ -86,7 +86,6 @@ if __name__ == '__main__':
 
         chrom_list=[chrom for chrom in chrom_list if chrom in bed_chroms]
 
-    args_dict=vars(args)
     chrom_lengths={}
     with open(args.ref_fai,'r') as file:
         for line in file:
@@ -107,7 +106,9 @@ if __name__ == '__main__':
             continue
 
         for mbase in range(1,chr_end,args.interval_len):
-            output_name='scatter_interval_%s_%d_%d.bed' %(chrom, mbase, min(chr_end,mbase+args.interval_len-1))
+            output_name='%s_%d_%d_scatter_interval.bed' %(chrom, mbase, min(chr_end,mbase+args.interval_len-1))
             out_path=os.path.join(args.output, output_name)
             with open(out_path,'w') as f:
                 f.write('%s\t%d\t%d' %(chrom, mbase, min(chr_end,mbase+args.interval_len-1)))
+            pysam.tabix_compress(out_path, out_path+'.gz')
+            pysam.tabix_index(out_path+'.gz', preset='bed')
